18724e02d2b937de338daed12c9141a2ea0ede45
AdanMc00 <hdceimolly@outlook.com>
2019-12-02 22:09:27 -0600

    primerejercicio

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log("hola")
+var pesosMx = 500
+var valorUsd = 19.57
+var valorEur = 26.57
+var valorCUC = 26.5 / 0.511285
+
+console.log(pesosMx / valorUsd)  
+console.log(pesosMx / valorEur)  
+console.log(pesosMx / valorCUC)  
