5be2e760e8e028207216ebda70456e77635fa29f
AdanMc00 <hdceimolly@outlook.com>
2019-12-03 15:09:39 -0600

    Segundo Ejercicio :ocotcat:

--- js/main.js
+++ js/main.js
@@ -6,3 +6,16 @@ var valorCUC = 26.5 / 0.511285
 console.log(pesosMx / valorUsd)  
 console.log(pesosMx / valorEur)  
 console.log(pesosMx / valorCUC)  
+
+
+
+var añosCumplidos = 30 
+var mesesCumplidos = 5
+var diasCumplidos = 12
+
+var totalDeDias = añosCumplidos*365 + mesesCumplidos*30 + diasCumplidos
+var horasTotales = totalDeDias * 24
+var minutosTotales = horasTotales * 60
+var segundosTotales = minutosTotales * 60 
+
+console.log(segundosTotales)
