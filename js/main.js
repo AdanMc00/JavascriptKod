dbada70573050751ee19d88ac8f51a96a66d05b0
AdanMc00 <hdceimolly@outlook.com>
2019-12-04 19:46:58 -0600

    inicio tercera clase

--- js/main.js
+++ js/main.js
@@ -2,3 +2,29 @@ var word = "perros"
 var wordMin = word[0] + word[1] +word[2] + word[3] +word [4]
 
 console.log(wordMin)
+
+var number = 15
+
+if ((number >= 10 && number < 100) && number % 10 === 10){
+    
+    console.log("el numero esta en el rango")
+}
+
+
+var word = "Vaca"
+
+if (word.length % 2 === 0) {console.log("EL numero es par")
+
+if (word.length >= 0){
+(console.log("el numero es mayo que cero") }
+
+ if  (word.length <=10) {
+    (console.log ("el numero es mayo que cero") }
+
+if (word.length >10) {
+    (console.log("el numero es mayo que cero")
+}
+
+else (<= 15) {
+    (console.log("el numero es mayo que cero")
+}
