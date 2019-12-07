a4dcadcdd2c1e5fd2357f9b78d38c7dfb7463cce
AdanMc00 <hdceimolly@outlook.com>
2019-12-06 19:20:56 -0600

    FourthClass

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-console.log("hola")
+var nombreCompleto = [["","Morachel"], ["jorge","camarillo"]]
+
+for (count = 0; count < nombreCompleto.length;count++ ){
+    nombreP = nombreCompleto[count] [0]
+    apellido = nombreCompleto [count] [1]
+    console.log(nombreP , apellido)
+    }
+    if (nombreP === ""){
+        nombreP = "_"
+        console.log(nombreP , apellido)
+    }
+
+    /*ejercicio 2*/
+
+    var input = ['p','e','r','r','o'] 
+    var word =""
+
+ 
+        
+    
+    
+    for (var count =0;count<input.length; count++){
+        output = input[count]
+        word += output
+    
+
+    }
+
+    
