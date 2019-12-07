7385f020b4a400223aa3f70bd5655f07b54367b6
AdanMc00 <hdceimolly@outlook.com>
2019-12-06 21:43:35 -0600

    fiveClass

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log("hola")
+function capitalize (string){
+    
+    string.toLowerCase()
+    var separateWords = string.split(' ')
+    var arrayCapitalize = []
+    for (count = 0; count< separateWords.length; count++) {
+        var wordToWord = separateWords[count]
+        var wordCapitalize =`${wordToWord[0].toUpperCase()} ${wordCapitalize.slice(1)}`
+        arrayCapitalize[count]= wordCapitalize
+    }
+
+    var arrayToString = arrayCapitalize.join (' ')
+    return arrayToString
+}
+
+
