460b428fe8de4372d0bc75683aaf3f36b15555c6
AdanMc00 <hdceimolly@outlook.com>
2019-12-09 17:05:44 -0600

    calse5 tarea

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 function capitalize (string){
     
     string.toLowerCase()
@@ -12,5 +13,35 @@ function capitalize (string){
     var arrayToString = arrayCapitalize.join (' ')
     return arrayToString
 }
+*/
 
+/*Ejercicio2*/
+
+function longestWord(phrase){
+
+
+    var word = phrase.split (" ")
+    var wordSplit = word[0]
+        for (count = 0 ; count<word.length - 1; count++){
+        var comparedWord = word[count+1]
+        if (comparedWord.length>wordSplit.length)
+        {wordSplit = comparedWord}
+   
+   return wordSplit
+   }
+}
+
+/* Ejercicio 3 */
+
+function numberone (matrix){
+var arrayJoin = matrix.join()
+var countNum = 0
+
+for(count = 0;count <arrayJoin.length; count++ ){
+    if (arrayJoin[count] === 1){
+        countNum++
+    }
+    return countNum
+}
+}
 
