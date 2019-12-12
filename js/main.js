87b0799a580f83050d4684d3a6561b1ab1c34b16
AdanMc00 <hdceimolly@outlook.com>
2019-12-11 21:24:06 -0600

    Class 8

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-console.log("hola")
+var alumno = [
+  {
+    nombre: "pedro",
+    apellido: "perez",
+    edad: 20,
+    calificacion: 9,
+  }, {
+    nombre: "luis",
+    apellido: "sanchez",
+    edad: 25,
+    calificacion:7,
+  }
+]
+
+
+
+  alumno.reduce((acc, valor) => acc + valor.calificacion / alumno.length),0
+  console.log(alumno)
+
+
+
+
+
