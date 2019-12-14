ec34e05babea62459670c9f62a5047b2a631e967
AdanMc00 <hdceimolly@outlook.com>
2019-12-13 21:54:36 -0600

    EjercicioClase10

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-ni
+/*Ejercici01*/
+var array = ['a','b','c','d','e']
+var list = document.getElementsByClassName("lista")
+array.forEach((key,index)=> {
+    list[index].innerText = key
+    })
+
+
