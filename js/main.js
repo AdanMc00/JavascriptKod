6744ef664a138272beb513a46598e6da21a7f157
AdanMc00 <hdceimolly@outlook.com>
2019-12-17 19:18:03 -0600

    EjercicioClase11

--- js/main.js
+++ js/main.js
@@ -1,1 +1,51 @@
-console.log("hola")
+/*var objectOne = [{
+  'nodename': 'p',
+  'content': 'hola'
+}, {
+  'nodename': 'p',
+  'content': 'mundo'
+}, {
+  'nodename': 'h1',
+  'content': 'perros'
+}
+]
+const buildStruct = (nodes, parent) => {
+
+  const conatiner = document.getElementById(parent)
+  nodes.forEach(
+    ({ nodename, content }) => {
+    const nodesTwo = document.createElement(nodename)
+    nodesTwo.innerText = content
+    container.appendChild(nodesTwo)
+    
+
+  })
+};
+*/
+
+/*Ejercicio2*/
+
+const arrayCards = [
+  {
+    'title': 'Koders aprenden',
+    'subtitle': 'a golpes',
+    'content': 'ndkjndkjn',
+    'img': 'tu imagen aqui'
+  }
+]
+const buildCards = (titulo, subtitulo, descripcion) => {
+
+
+  return (
+    <div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">$(title)></h5>
+        <h6 class="card-subtitle mb-2 text-muted">$(subtitle)</h6>
+        <p class="card-text">$(description)</p>
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
+      </div>
+    </div>
+}
+
+
