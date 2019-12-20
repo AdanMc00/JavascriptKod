0c0363858b6ead5a044b2041595255e2805cb00f
AdanMc00 <hdceimolly@outlook.com>
2019-12-19 19:52:30 -0600

    EjercicioClase13

--- js/main.js
+++ js/main.js
@@ -1,51 +1,18 @@
-/*var objectOne = [{
-  'nodename': 'p',
-  'content': 'hola'
-}, {
-  'nodename': 'p',
-  'content': 'mundo'
-}, {
-  'nodename': 'h1',
-  'content': 'perros'
-}
-]
-const buildStruct = (nodes, parent) => {
+var menu = ["perro1", "perro2", "perro3"]
 
-  const conatiner = document.getElementById(parent)
-  nodes.forEach(
-    ({ nodename, content }) => {
-    const nodesTwo = document.createElement(nodename)
-    nodesTwo.innerText = content
-    container.appendChild(nodesTwo)
-    
-
-  })
-};
-*/
-
-/*Ejercicio2*/
-
-const arrayCards = [
-  {
-    'title': 'Koders aprenden',
-    'subtitle': 'a golpes',
-    'content': 'ndkjndkjn',
-    'img': 'tu imagen aqui'
-  }
-]
-const buildCards = (titulo, subtitulo, descripcion) => {
-
-
-  return (
-    <div class="card" style="width: 18rem;">
-      <div class="card-body">
-        <h5 class="card-title">$(title)></h5>
-        <h6 class="card-subtitle mb-2 text-muted">$(subtitle)</h6>
-        <p class="card-text">$(description)</p>
-        <a href="#" class="card-link">Card link</a>
-        <a href="#" class="card-link">Another link</a>
-      </div>
-    </div>
+const buildMenu = (arr, parentSelector) => {
+  let parent = document.getElementsByTagName(parentSelector)[0]
+  let children = menu.reduce((acc, cuValue )=>'${acc}<li class ="test">${cuValue}</li>','')
+  parent.innerHTML = children
 }
 
+buildMenu(menu,'ul')
 
+var liNodes = document.getElementsByTagName('li')
+
+
+for(let index = 0;index<liNodes.length; index++){
+liNodes[index].addEventListener("click", ()=>{
+  console.log('aqui ${index}')
+})
+}
