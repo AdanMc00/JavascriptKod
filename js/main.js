e0d19fad2ccd83186e80c93e691a96195f1a4e54
AdanMc00 <hdceimolly@outlook.com>
2019-12-19 22:12:38 -0600

    EjercicioClase14

--- js/main.js
+++ js/main.js
@@ -1,18 +1,42 @@
-var menu = ["perro1", "perro2", "perro3"]
+/*
 
-const buildMenu = (arr, parentSelector) => {
-  let parent = document.getElementsByTagName(parentSelector)[0]
-  let children = menu.reduce((acc, cuValue )=>'${acc}<li class ="test">${cuValue}</li>','')
-  parent.innerHTML = children
-}
-
-buildMenu(menu,'ul')
-
-var liNodes = document.getElementsByTagName('li')
-
-
-for(let index = 0;index<liNodes.length; index++){
-liNodes[index].addEventListener("click", ()=>{
-  console.log('aqui ${index}')
+var person = JSON.stringify({
+  "age": 30,
+  "firstName": "Adan",
+  "lastName": "Mijangos",
+  "pitch": false
 })
+
+var response
+var xhttp = new XMLHttpRequest()
+xhttp.onreadystatechange = function () {
+  if (this.readyState === 4 && this.status === 200) {
+
+    response = this.response
+  }
 }
+
+xhttp.open("POST","https://ajaxkode.firebaseio.com/koders.json")
+xhttp.send(person)
+*/
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () {
+  if (this.readyState === 4 && this.status === 200) {
+    
+    responseHandler(this.response) 
+  }
+}
+
+
+
+xhttp.open("GET","https://ajaxkode.firebaseio.com/.json")
+xhttp.send()
+
+const responseHandler= response => {
+  var responseObj = JSON.parse(response)
+  var kodersMatrix = Object.entries(responseObj)
+  
+  console.log(kodersMatrix)
+}
+
